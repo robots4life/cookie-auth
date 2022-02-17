ebcc675be976d6a94e606f5199f3d84b91baaa58
robots4life <34193243+robots4life@users.noreply.github.com>
2022-02-17 17:21:40 +0100

    create Login & Register components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,15 @@
-<h1>Authentication with Cookies in SvelteKit</h1>
+<script>
+	import Login from '$lib/Login.svelte';
+	import Register from '$lib/Register.svelte';
+	import { goto } from '$app/navigation';
+
+	function redirectToProfile() {
+		goto('/profile');
+	}
+</script>
+
+<main>
+	<h1>Authentication with Cookies in SvelteKit</h1>
+	<Login on:success="{redirectToProfile}" />
+	<Register on:success="{redirectToProfile}" />
+</main>
