9e62b72a7e30eac18695e1be277b8607e1401094
robots4life <34193243+robots4life@users.noreply.github.com>
2022-02-17 17:28:04 +0100

    prepare Login & Register

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -1,1 +1,15 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	let email;
+	let password;
+
+	async function login() {}
+</script>
+
 <h1>Login</h1>
+<input type="email" bind:value="{email}" placeholder="Enter your Email" />
+<input type="password" bind:value="{password}" placeholder="Enter your Password" />
+<button on:click="{login}">Login</button>
--- src/lib/Register.svelte
+++ src/lib/Register.svelte
@@ -1,1 +1,17 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	let name;
+	let email;
+	let password;
+
+	async function register() {}
+</script>
+
 <h1>Register</h1>
+<input type="text" bind:value="{name}" placeholder="Enter your Name" />
+<input type="email" bind:value="{email}" placeholder="Enter your Email" />
+<input type="password" bind:value="{password}" placeholder="Enter your Password" />
+<button on:click="{register}">Register</button>
