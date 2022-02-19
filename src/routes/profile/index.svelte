a7b3f2226fa5bd907a2f3a0387a425f13800a053
robots4life <34193243+robots4life@users.noreply.github.com>
2022-02-19 17:41:47 +0100

    access session in endpoints, not svelte files

--- src/routes/profile/index.svelte
+++ src/routes/profile/index.svelte
@@ -1,16 +1,3 @@
-<script>
-	// https://kit.svelte.dev/docs/modules#$app-stores
-	// Stores are contextual — they are added to the context of your root component.
-	// This means that session and page are unique to each request on the server,
-	// rather than shared between multiple requests handled by the same server simultaneously,
-	// which is what makes it safe to include user-specific data in session.
-	//
-	// session from hooks.js is stored in $app/stores and available on every endpoint
-	import { session } from '$app/stores';
-</script>
-
-{$session.user}
-
 <h1>Profile</h1>
 
 <style>
